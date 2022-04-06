31d3b5271cc10b314896503584b483e000378236
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-06 09:01:50 +0700

    add: signup and autologin after signup

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta name="msapplication-TileColor" content="#FFFFFF">
     <meta name="msapplication-tap-highlight" content="no">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" media="screen, projection">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" media="screen, projection">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
@@ -50,8 +51,9 @@
     <div id="modal-signin" class="modal">
         <div class="modal-content">
             <h4>Form Login</h4>
+
             <div class="row">
-                <form id="form-login" class="col s12" method="post">
+                <form id="form-login" class="col s12" method="post" autocomplete="off">
                     <div class="row">
                         <div class="input-field col s12">
                             <input id="username" name="username" type="text" class="validate" required pattern="[a-zA-Z0-9\s]{3,20}" minlength="3" maxlength="20">
@@ -69,14 +71,50 @@
             </div>
         </div>
         <div class="modal-footer">
-            <button onclick="$('#login').click()" class="btn waves-effect waves-light" type="submit" name="action">
+            <a href="#!" class="modal-close waves-effect waves-red btn-flat">Cancel</a>
+            <a href="#!" onclick="$('#login').click()" class="btn blue white-text waves-effect waves-light btn-flat">
                 Login
-            </button>
+            </a>
+        </div>
+    </div>
+    <div id="modal-signup" class="modal modal-fixed-footer">
+        <div class="modal-content">
+            <h4>Form Signup</h4> <hr>
+            <div class="row">
+                <form id="form-signup" class="col s12" method="post" autocomplete="off">
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="name" name="name" type="text" class="validate" required pattern="[a-zA-Z0-9\s]{3,20}" minlength="3" maxlength="20">
+                            <label for="name">Full Name</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="uname" name="username" type="text" class="validate" required pattern="[a-zA-Z0-9\s]{3,20}" minlength="3" maxlength="20">
+                            <label for="uname">Username</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="pwd" name="password" type="password" class="validate" required pattern="[a-zA-Z0-9\s]{3,20}" minlength="3" maxlength="20">
+                            <label for="pwd">Password</label>
+                        </div>
+                    </div>
+                    <button id="btn-signup" type="submit" hidden></button>
+                </form>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-red btn-flat">Cancel</a>
+            <a href="#!" onclick="$('#btn-signup').click()" class="btn blue white-text waves-effect waves-light btn-flat">
+                Signup
+            </a>
         </div>
     </div>
 </body>
 <script type="text/javascript">
     $(document).ready(function() {
+
         const APPURL = "pendek.id", API = "http://localhost:1000";
         
         init();
@@ -136,14 +174,12 @@
                 }
             })
         });
-        $("#form-login").submit(function(event) {
-            event.preventDefault();
-            M.toast({html: 'Please wait...', classes: 'rounded'});
-            $.ajax({
+        async function login(username, password) {
+            await $.ajax({
                 url: API+"/user/login",
                 data: JSON.stringify({
-                    "username": $("#username").val(),
-                    "password": $("#password").val()
+                    "username": username,
+                    "password": password
                 }),
                 headers: {
                     "Content-Type": 'application/json'
@@ -159,11 +195,46 @@
                     setItemWithExpiry('username', $("#username").val());
                     setItemWithExpiry('name', name);
                     init();
-                    $("#modal-signin").modal('close');
+                    $("#modal-signin, #modal-signup").modal('close');
                 }
             }).fail((xhr, status, err) => {
                 if (xhr.status == 404 || xhr.status == 400) {
                     M.toast({html: 'Username or password is wrong!', classes: 'rounded'});
+                }
+            })
+        }
+        $("#form-login").submit(function(event) {
+            event.preventDefault();
+            M.toast({html: 'Please wait...', classes: 'rounded'});
+            const username = $("#username").val(), password = $("#password").val();
+            login(username, password);
+        });
+        $("#form-signup").submit(function(event) {
+            event.preventDefault();
+            M.toast({html: 'Please wait...', classes: 'rounded'});
+            $.ajax({
+                url: API+"/user/signup",
+                data: JSON.stringify({
+                    "name": $("#name").val(),
+                    "username": $("#uname").val(),
+                    "password": $("#pwd").val()
+                }),
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+                dataType: "json",
+                type: "post"
+            }).done((data, status, xhr) => {
+                if (data.status == 201) {
+                    M.toast({html: 'Congratulations, your account has been successfully created!', classes: 'rounded'});
+                    M.toast({html: 'Login... please wait', classes: 'rounded'});
+                    setTimeout(function(){
+                        login($("#uname").val(), $("#pwd").val());
+                    }, 1000);
+                }
+            }).fail((xhr, status, err) => {
+                if (xhr.status == 404 || xhr.status == 400) {
+                    M.toast({html: 'Bad Request!', classes: 'rounded'});
                 }
             })
         });
